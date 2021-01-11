3eaae5f1df4108578b7a9bec5f45e840e958129d
Winnie Chou <winnie.chou.mb@gmail.com>
2021-01-11 14:29:26 -0600

    step 2 user model in process. Completed up to messagesFrom query

--- models/user.js
+++ models/user.js
@@ -1,5 +1,10 @@
 "use strict";
 
+const db = require("../db");
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+const { NotFoundError } = require("../expressError");
+
 /** User of the site. */
 
 class User {
@@ -9,22 +14,66 @@ class User {
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const result = await db.query(
+      `INSERT INTO users (username,
+                          password,
+                          first_name,
+                          last_name,
+                          phone,
+                          join_at)
+        VALUES
+        ($1, $2, $3, $4, $5, current_timestamp)
+        RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone]);
+
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const result = await db.query(
+      `SELECT username,
+              password
+        FROM users
+        WHERE username = $1`,
+       [username]);
+    const user = result.rows[0];
+
+    if (user) {
+      if (await bcrypt.compare(password, user.password) === true) {
+        return true;
+      }
+    }
+    return false;
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE users
+       SET last_login_at = current_timestamp
+         WHERE username = $1
+         RETURNING id, read_at`,
+       [username]);
+    
+    // QUESTION: Is there a pro coding way to ensure this is done besides testing? (i.e. console.log message)
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    const results = await db.query(
+      `SELECT username,
+              first_name,
+              last_name
+        FROM users`
+      );
+    
+    return results.row;
   }
 
   /** Get: get user by username
@@ -37,6 +86,22 @@ class User {
    *          last_login_at } */
 
   static async get(username) {
+    const result = await db.query(
+      `SELECT username,
+              password,
+              first_name,
+              last_name,
+              phone,
+              join_at,
+              last_login_at
+         FROM users
+         WHERE username = $1`,
+       [username]);
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`No such user: ${username}`);
+
+    return user;
   }
 
   /** Return messages from this user.
@@ -48,6 +113,45 @@ class User {
    */
 
   static async messagesFrom(username) {
+    const results = await db.query(
+      `SELECT m.id,
+              m.from_username,
+              f.first_name AS from_first_name,
+              f.last_name AS from_last_name,
+              f.phone AS from_phone,
+              m.to_username,
+              t.first_name AS to_first_name,
+              t.last_name AS to_last_name,
+              t.phone AS to_phone,
+              m.body,
+              m.sent_at,
+              m.read_at
+        FROM messages AS m
+        JOIN users AS f ON m.from_username = f.username
+        JOIN users AS t ON m.to_username = t.username
+        WHERE f.username = $1`,
+        [username]);
+    const messages = results.rows;
+    console.log("messages = ", messages);
+
+    if (!messages) throw new NotFoundError(`No messages for user: ${username}`);
+
+    return messages.map( m => {
+      {
+        id: m.id,
+        to_user: {
+          username: m.to_username,
+          first_name: to_first_name,
+          last_name: to_last_name,
+          phone: to_phone
+        },
+        body: m.body,
+        sent_at: m.sent_at,
+        read_at: m.read_at
+      };
+    });
+
+    }
   }
 
   /** Return messages to this user.
