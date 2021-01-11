071c98aabb31c80026bc02f12a7b9ad1e8d0679c
Winnie Chou <winnie.chou.mb@gmail.com>
2021-01-11 17:46:55 -0600

    Step 4: user routes completed

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@
 
 const Router = require("express").Router;
 const router = new Router();
+const User = require("../models/user.js");
 
 
 /** GET / - get list of users.
@@ -10,12 +11,22 @@ const router = new Router();
  *
  **/
 
+router.get("/", async function (req, res, next) {
+  const users = await User.all();
+  return res.send({ users });
+});
+
 
 /** GET /:username - get detail of users.
  *
  * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
  *
  **/
+
+router.get("/:username/", async function (req, res, next) {
+  const user = await User.get(req.params.username);
+  return res.send({ user });
+});
 
 
 /** GET /:username/to - get messages to user
@@ -28,6 +39,10 @@ const router = new Router();
  *
  **/
 
+router.get("/:username/to", async function (req, res, next) {
+  const messages = await User.messagesTo(req.params.username);
+  return res.send({ messages });
+});
 
 /** GET /:username/from - get messages from user
  *
@@ -38,5 +53,10 @@ const router = new Router();
  *                 to_user: {username, first_name, last_name, phone}}, ...]}
  *
  **/
+
+router.get("/:username/from", async function (req, res, next) {
+  const messages = await User.messagesFrom(req.params.username);
+  return res.send({ messages });
+});
 
 module.exports = router;
