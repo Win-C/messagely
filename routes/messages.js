970104c9c4d94c8761eebdca3fc0e37f71408568
Tracy Jiang <tracyjiangmx@gmail.com>
2021-01-11 16:36:25 -0800

    started working on message routes

--- routes/messages.js
+++ routes/messages.js
@@ -1,7 +1,10 @@
-"use strict";
+'use strict';
 
-const Router = require("express").Router;
+const Router = require('express').Router;
 const router = new Router();
+const { ensureLoggedIn } = require('../middleware/auth.js');
+const Message = require('../models/message.js');
+const { UnauthorizedError } = require('../expressError');
 
 /** GET /:id - get detail of message.
  *
@@ -16,6 +19,16 @@ const router = new Router();
  *
  **/
 
+router.get('/:id/', ensureLoggedIn, async function(req, res, next) {
+	const message = await Message.get(req.params.id);
+	const from_username = res.locals.user.username;
+
+	if (message.from_user.username !== from_username && message.to_user.username !== from_username) {
+		throw new UnauthorizedError(`You are not authorized to view the message!`);
+	}
+
+	return res.send({ message });
+});
 
 /** POST / - post message.
  *
@@ -24,6 +37,13 @@ const router = new Router();
  *
  **/
 
+router.post('/', ensureLoggedIn, async function(req, res, next) {
+	const { to_username, body } = req.body;
+	const from_username = res.locals.user.username;
+	const message = Message.create({ from_username, to_username, body });
+
+	return res.json({ message });
+});
 
 /** POST/:id/read - mark message as read:
  *
@@ -33,5 +53,23 @@ const router = new Router();
  *
  **/
 
+router.post('/:id/read', ensureLoggedIn, async function(req, res, next) {
+	const { id } = req.params.id;
+	const currentUser = res.locals.user.username;
+	const message = Message.get(id);
+
+	Message.markRead({ id });
+
+	if (currentUser !== message.to_user.username) {
+		throw new UnauthorizedError(`You are not authorized to read the message!`);
+	}
+
+	return res.json({
+		message: {
+			id: message.id,
+			read_at: message.read_at
+		}
+	});
+});
 
 module.exports = router;
